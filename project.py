9d196ba91d182781e840a9b0a2ebe2c9e4fd5345
Seunghun Lee <waydi1@gmail.com>
2016-11-08 00:56:06 -0800

    Add route to new, edit and delete menu items

--- project.py
+++ project.py
@@ -30,6 +30,20 @@ def restaurant_menu(restaurant_id):
     return output
 
 
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
