66b5c191d0e34ce52bc6d647d7879b302e1cdb26
Seunghun Lee <waydi1@gmail.com>
2016-11-07 16:44:16 -0800

    Update webserver.py
    Implement reading restaurant from database.

--- webserver.py
+++ webserver.py
@@ -1,5 +1,15 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class WebServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -9,14 +19,13 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.end_headers()
 
             output = '<html><body>'
-            output += 'hello'
-            output += '<h2>What would you like me to say?</h2>'
-            output += '''
-                      <form method="post" enctype="multipart/form-data" action="/hello">
-                        <input type="text" name="message">
-                        <input type="submit">
-                      </form>
-                      '''
+
+            restaurants = session.query(Restaurant).all()
+
+            for restaurant in restaurants:
+                output += '<h2>%s</h2>' % restaurant.name
+
+            output += '</body></html>'
             self.wfile.write(output)
             print output
         else:
